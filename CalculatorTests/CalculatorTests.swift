c39aecafcb18dcb0f0ab828d584ad8e31c862869
mgKhantt <khantphonenaing156@gamil.com>
2024-11-03 13:59:38 +0630

    Initial Commit

---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Khant Phone Naing  on 03/11/2024.
+//
+
+import Testing
+@testable import Calculator
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
