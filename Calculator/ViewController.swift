5410edae79fca1c6dc86e6791c79fba471b9eeaa
mgKhantt <khantphonenaing156@gamil.com>
2024-11-03 16:48:40 +0630

    Logic Complete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var displayLabel: UILabel!
+    
+    var currentNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var currentOperation:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        displayLabel.text = "0"
     }
 
-
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if let numberText = sender.titleLabel?.text {
+            currentNumber += numberText
+            displayLabel.text = currentNumber
+        }
+    }
+    
+    @IBAction func operatorPressed(_ sender: UIButton) {
+        if let operation = sender.titleLabel?.text {
+            firstOperand = currentNumber
+            currentNumber = ""
+            currentOperation = operation
+        }
+    }
+    
+    
+    @IBAction func equalPressed(_ sender: UIButton) {
+        secondOperand = currentNumber
+        
+        guard let firstValue = Double(firstOperand), let secondValue = Double(secondOperand), let operation = currentOperation else {
+                return
+            }
+        
+        var result : Double = 0
+        
+        switch operation {
+        case "+":
+            result = firstValue + secondValue
+        case "-":
+            result = firstValue - secondValue
+        case "×":
+            result = firstValue * secondValue
+        case "÷":
+            result = secondValue != 0 ? firstValue / secondValue : 0
+        default:
+            break
+        }
+        
+        displayLabel.text = String(result)
+        currentNumber = ""
+        firstOperand = ""
+        secondOperand = ""
+        currentOperation = nil
+    }
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        currentNumber = ""
+        firstOperand = ""
+        secondOperand = ""
+        currentOperation = nil
+        displayLabel.text = "0"
+    }
+    
 }
 
